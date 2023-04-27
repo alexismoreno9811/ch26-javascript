1b46f081d9b1342614dcb4ad22eae2def183dc2b
Alexis Moreno <alexis.morenotrejo@gmail.com>
2023-04-27 16:25:44 -0600

    ci: ciclo do-while

--- js04/js04.js
+++ js04/js04.js
@@ -19,9 +19,9 @@ console.log(apellidosCh26);
 // [ [ ], [ ], [ ] ]
 
 const personasEnCohorte = [
-/* ch26 */  ["Arath"  , "José"    , "Karen"    , "Sofi"         ],
-/* ch27 */  ["Manuel" , "María"   , "Francisco", "Pablo", "Pepe"],
-/* ch28 */  ["Doris"  , "Brendan" , "Lucas"                     ]
+/* ch26 */["Arath", "José", "Karen", "Sofi"],
+/* ch27 */["Manuel", "María", "Francisco", "Pablo", "Pepe"],
+/* ch28 */["Doris", "Brendan", "Lucas"]
 ];
 
 console.log(`Personas ch27: ${personasEnCohorte[1].join(", ")}`);
@@ -48,4 +48,104 @@ console.log(`Personas ch27: ${personasEnCohorte[1].join(", ")}`);
 } */
 
 // Usando forEach
-personasEnCohorte.forEach(fila => fila.forEach(columna => console.log(columna) ) );
+personasEnCohorte.forEach(fila => fila.forEach(columna => console.log(columna)));
+
+// forOf
+// Ejecuta sentencias por cada elemento de un objeto iterable (arrays, strings y objetos)
+const myName = "Alexis";
+for (let char of myName) {
+  console.log(char);
+}
+
+for (const fila of personasEnCohorte) {
+  for (const columna of fila) {
+    console.log(columna);
+  }
+}
+
+// --------------------- Consideraciones para el ciclo For -------------------------
+// No es necesario indicar el inicio, la comparación y la expresión final
+/* 
+  for (inicio; comparación; expresiónFinal){
+    sentencias;
+  } 
+*/
+// break; Rompe con la iteración en curso y sale del ciclo.
+let nIteración = 0;
+for ( /*inicio*/; /*comparación*/; /*ExpFinal*/){
+  console.log(nIteración)
+  if (nIteración === 5) break;
+  nIteración++;
+}
+
+//----------------- Ejercicio -------------------------
+// Iterar y mostrar a las personas en personasEnCohorte y detenr cuando se encuentre a María.
+// Dejar de iterar.
+
+personas: // Se usa un label para indicar en el brak hacia que ciclo romperá.
+for (let fila = 0; fila < personasEnCohorte.length; fila++) {
+  for (let columna = 0; columna < personasEnCohorte[fila].length; columna++){
+    console.log(personasEnCohorte[fila][columna]);
+    if (personasEnCohorte[fila][columna] == "María") break personas; // Se indica el label
+  }
+}
+
+// Continue
+/* 
+  termina la ejecución de la sentencia en la iteración actual y continua la ejecución del ciclo
+  con la próxima iteración (o en el label que se indique).
+*/
+// No imprimir María.
+console.log("===== uso de continue =======")
+personas:
+for (let fila = 0; fila < personasEnCohorte.length; fila++) {
+  for (let columna = 0; columna < personasEnCohorte[fila].length; columna++){
+    if (personasEnCohorte[fila][columna] == "María") continue;
+    console.log(personasEnCohorte[fila][columna]);
+  }
+}
+
+// ------------------------- Ciclo While ----------------------
+/* 
+  Crea un bucle que ejecuta una sentencia mientras la condición especificada sea verdadera.
+
+  Sintaxis:
+  while( Condición )
+    Sentencias;
+    
+let counter = 0;
+while (confirm("¿Quieres continuar?")) {
+  console.log(`Has continuado ${++counter} ${counter > 1 ? "veces" : "vez" }`);
+} */
+
+const numbers = [9, 5, 6, 7];
+let i = 0;
+while (i < numbers.length) {
+  console.log(numbers[i]);
+  i++;
+}
+
+// --------------------------- Práctica -------------------
+/* 
+
+
+*/
+
+// --------------------------- Ciclo do while ---------------
+/* 
+  Crea un bucle que ejecuta una sentencia hasta que la condición de comprobación se evalue 
+  como falso. La condición se evalua después de ejecutar la sentencia, por lo que la sentencia
+  se ejecuta por lo menos una vez.
+
+  Sintaxis
+
+  do{
+    sentencias;
+  } while( comprobación );
+ 
+*/
+let iteration = 0;
+do {
+  iteration++;
+  console.log(`Número de iteración do-while: ${iteration}`);
+} while (confirm(`¿Deseas continuar?`));
