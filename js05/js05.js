b2c9af4d8c04c022bd35762414246aadc28449f5
Alexis Moreno <alexis.morenotrejo@gmail.com>
2023-04-28 10:29:27 -0600

    ci: Operadores aritmeticos y de asignación

--- js05/js05.js
+++ js05/js05.js
@@ -1,6 +1,27 @@
 console.log(`Sesion js05 operadores`);
 
 // --------------- Operadores aritmeticos -----------
-//
+// Adición +
+// Multiplicación *
+// Substracción -
+// División /
+// Exponencial **
+// Resto %
 
+// Ejemplo
+let dinero = 20 + 10; // 30
 
+// --------------- Operadores de asignación -----------
+// Asigna un valor a su operando izquierdo basandose en el valor de
+// su operando derecho
+// Asignación =
+// Asignación de adición +=
+dinero += 15; // dinero = dinero + 15;
+// Asignación de multiplicación *=
+// Asignación de substracción -=
+// Asignación de división /=
+// Asignación de exponencial **=
+// Asignación de resto %=
+12 + 7 + 12 + 4 + 1;
+
+2 ** 3 ** 2;
