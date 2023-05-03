18e7b9218742e78948835abcce122b21b87fe581
Alexis Moreno <alexis.morenotrejo@gmail.com>
2023-05-03 12:14:13 -0600

    ci: Manipulación del dom arrow functions

--- js06/js06.js
+++ js06/js06.js
@@ -1,1 +1,22 @@
 console.log(`Sesion js06 Manipulación del DOM`);
+
+const findElementById = (nameOfDeveloper) => {
+  const tituloRef = document.getElementById(`titulo`);
+  tituloRef.innerHTML += `, ${nameOfDeveloper}`;
+};
+findElementById(`Alexis`);
+
+const changeSubtitleById = (newTxt) => {
+  const subtituleRef = document.getElementById(`subtitulo`);
+  subtituleRef.innerHTML = `${subtituleRef.innerText}, <em>${newTxt}</em>`;
+};
+changeSubtitleById(`Viernes de cholos`);
+
+// ----------------------- Encontrar elementos por su tag -------------------
+// getElementsByTagName()
+
+const changeEmphasis = (newTxt) => {
+  
+
+};
+changeEmphasis(`Viernes friki`);
